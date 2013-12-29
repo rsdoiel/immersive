56ad5ee88c203525675c01f959b9b2f95cc3bfb8
R. S. Doiel <rsdoiel@gmail.com>
2013-12-28 16:42:41 -0800

    snapshot

--- cl-immersive.lisp
+++ cl-immersive.lisp
@@ -4,12 +4,15 @@
 ;; Most prior work done in Unix and still provide a rich Lisp environment to
 ;; build application and explore your Raspberry Pi.
 ;;
-
 (defun cl-immersive (&optional (action "load"))
   "Args: action - can be load, lisp or compile
   Eventually this will allow us to optimise what happens easily from .eclrc"
   (progn
-    (or (equal action "load") (load "cl-immersive/setup.lisp"))
-    (or (equal action "lisp") (load "cl-immersive/setup"))
-    (or (equal action "compile") (compile-file "cl-immersive/setup.lisp"))))
+    (or (equal action "load") (load "cl-immersive/setup"))
+    (or (equal action "lisp") (load "cl-immersive/setup.lisp"))
+    (if (equal action "compile") 
+      (progn
+	(compile-file "cl-immersive/setup.lisp")
+	(load "cl-immersive/setup")))
+    (cl-immersive-setup action))
 
--- cl-immersive/setup.lisp
+++ cl-immersive/setup.lisp
@@ -1,19 +1,66 @@
-;;
-;; Scan this path and load the lisp or fas file we find.
-;;
-(defun cl-immersive-pathname-to-string (filename-as-pathname)
+;;;
+;;; cl-immserive/setup.lisp - bootstrap my immersive lisp environment for 
+;;; _ecl_ and _Raspbery Pi_
+;;;
+;;; Setup an immersive Lisp environment for Raspberry Pi
+;;; Hosted by the [ecl]() Lisp implementation, cl-immersive will be able to leverage
+;;; Most prior work done in Unix and still provide a rich Lisp environment to
+;;; build application and explore your Raspberry Pi.
+;;;
+
+
+;; Convert a pathname type to a string type (optionally minus the filename extention),
+;; #P and the quotes.
+(defun cl-immersive-pathname-to-string (filename-as-pathname &optional (extname nil))
+  "Args: filename-as-pathname, extname (defaults to nil)
+     
+    + filename-as-pathname would be the type returned for (directory ...)
+    + extname would be a string such as _.lisp_ including the leading period or nil
+    if the extention needs to remain untouched.
+
+   Returns a string version of the pathname."
   (let ((fname-string nil))
     (progn
       (setq fname-string (format nil "~S" filename-as-pathname))
-      (subseq fname-string 3 (- (length fname-string) 6)))))
+      (if extname
+	(subseq fname-string 3 (- (length fname-string) (1+ (length extname))))
+	(subseq fname-string 3 (1- (length fname-string)))))))
 
-(defun cl-immersive-smart-load (filename-as-pathname)
-  (let ((fname-string (cl-immersive-pathname-to-string filename-as-pathname)))
-      (load fname-string)))
+;;
+;; Load or Compile then load a file based on an intention.
+;; The intentions understood are to load what's available
+;; preferring the .fas file, explicity load the .lisp version
+;; or compile the .lisp version and load the resulting .fas file.
+;;
+(defun cl-immersive-smart-load (fname-string intention)
+  "Args: fname-string, intention
 
-; Scan the directory and get a list of filenames
-(defun cl-immersive-setup ()
-  ; With list of filenames load, trim .lisp and them
-  (dolist (filename-as-pathname (directory (concatenate 'string (ext:getenv "HOME") "/cl-immersive/*.lisp")))
-    (cl-immersive-smart-load filename-as-pathname)))
+    + fname-string would be a string version of a pathname *without a filename extention*.
+    + intention is a string of either "compile", "lisp" or "load".
+
+  Returns the results of (load ...)"
+    (progn
+      (or (equal intention "compile") (compile-file fname-string))
+      (if (equal intention "lisp")
+	(load (concatenate 'string fname-string ".lisp"))
+	;; defautl to load either .fas or .lisp as available
+	(load fname-string))))
+
+;;
+;; Scan this cl-immersive folder and load the lisp or fas files we find.
+;; If requested compile them first.
+;;
+(defun cl-immersive-setup (&optional (action "load"))
+  "Args: action is a string that is either load, lisp, compile. The default is load.
+  	load - will load either .fas or if not found the .lisp file
+  	lisp - will explicitly load the .lisp files only
+  	compile - will compile the lisp files and then load the .fas rendered.
+  Returns results of loadnig the list of files."
+  (let ((fname-as-string nil))
+    ; Now process all the parts, make a list of files and compile or load them
+    (dolist (filename-as-pathname (directory (concatenate 'string (ext:getenv "HOME" "/cl-immersive/*.lisp"))))
+      (setq fname-as-string (cl-immersive-pathname-to-string filename-as-pathname ".lisp"))
+      ;FIXME: don't bother loading with setup.lisp
+      (format t "DEBUG filename-as-string ~S~%" fname-as-string)
+      (cl-immersive-smart-load fname-as-string action))))
 
