cd5be23279fd0fc7065539dd0db193ff2f053399
R. S. Doiel <rsdoiel@gmail.com>
2013-12-29 15:11:27 -0800

    snapshot

--- cl-immersive/git-scm.lisp
+++ cl-immersive/git-scm.lisp
@@ -1,14 +1,14 @@
 ;;;
-;;; git-cmds.lisp - quick and dirty wrapper for git to use from 
+;;; git-scm.lisp - quick and dirty wrapper for git to use from 
 ;;; _ecl_ lisp.
 ;;;
 ;;; Author: R. S. Doiel, <rsdoiel@yahoo.com>
 ;;;
 
 ;;
-;; git-cmds - wrap the basic git command.
+;; git-scm - wrap the basic git command.
 ;;
-(defun git-cmds (action &optional (arg1 " ") (arg2 " ") (arg3 " ") (arg4 " ") (arg5 " "))
+(defun git-scm (action &optional (arg1 " ") (arg2 " ") (arg3 " ") (arg4 " ") (arg5 " "))
   (let ((cmd nil))
     ;FIXME: turn a symbol into a string for the 'action' element.
     (setq cmd (string-trim " " (concatenate 'string "git " action " " arg1 " " arg2 " " arg3 " " arg4 " " arg5)))
@@ -21,29 +21,35 @@
 ;; git-status - run git status on the repo.
 ;;
 (defun git-status ()
-  (git-cmds "status"))
+  (git-scm "status"))
 
 ;;
 ;; git-add - Add a file to the git repo.
 ;;
 (defun git-add (fname)
-  (git-cmds "add" fname))
+  (git-scm "add" fname))
 
 ;;
 ;; git-commit - Commit the current state of development.
 ;;
 (defun git-commit (&optional (msg "snapshot"))
-  (git-cmds "commit -am" (concatenate 'string "\""  msg "\"")))
+  (git-scm "commit -am" (concatenate 'string "\""  msg "\"")))
 
 ;;
 ;; git-push - Push the current state to master
 ;;
 (defun git-push (&optional (source "origin") (branch "master"))
-  (git-cmds "push" source branch)) 
+  (git-scm "push" source branch)) 
 
 ;;
 ;; git-pull - Pull the current state from master
 ;;
 (defun git-pull (&optional (source "origin") (branch "master"))
-  (git-cmds "pull" source branch))
+  (git-scm "pull" source branch))
+
+;;
+;; git-clone - clone a remote repo
+;;
+(defun git-clone (repos-url) 
+  (git-scm "clone " (concatenate 'string "\""  repos-url "\"")))
 
