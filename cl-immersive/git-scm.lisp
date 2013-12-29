0c0e5ba9633549b2f83dfdc553a89837c3d06b7c
R. S. Doiel <rsdoiel@gmail.com>
2013-12-29 15:23:30 -0800

    snapshot

--- cl-immersive/git-scm.lisp
+++ cl-immersive/git-scm.lisp
@@ -40,14 +40,23 @@
 ;;
 ;; git-push - Push the current state to master
 ;;
-(defun git-push (&optional (source "origin") (branch "master"))
+(defun git-push (&optional (source " ") (branch " "))
   (git-scm "push" source branch)) 
 
 ;;
 ;; git-pull - Pull the current state from master
 ;;
-(defun git-pull (&optional (source "origin") (branch "master"))
+(defun git-pull (&optional (source " ") (branch " "))
   (git-scm "pull" source branch))
+
+;;
+;; git-pull-origin-master - short cut to execute git pull origin master
+;;
+(defun git-pull-origin-master () (git-pull "origin" "master"))
+
+;;
+;; git-push-orgin-master - shortgut to push to origin master
+(defun git-push-origin-master () (git-push "origin" "master"))
 
 ;;
 ;; git-clone - clone a remote repo
