458d8f08093dce4333511091cb17649295145063
R. S. Doiel <rsdoiel@gmail.com>
2013-12-31 12:03:07 -0800

    Added a simple wrapper for editing then updating called update-ed

--- cl-immersive/cd.lisp
+++ cl-immersive/cd.lisp
@@ -6,6 +6,14 @@
   CD calls ext:chdir to change the working directory. CD without args should
   change the directory back to your $HOME location."
   (progn
-    (ext:chdir pathname)
-    (ext:getcwd)))
+    (if (eq (type-of pathname) 'SYMBOL)
+      (progn
+	(print "turning symbol into string")
+	(setq pathname (symbol-name pathname))
+	(print pathname)
+	(print (type-of pathname))
+	(print "DEBUG now to find the right case..."))
+      (progn
+	(ext:chdir pathname)
+	(ext:getcwd)))))
 
---
+++ cl-immersive/update-ed.lisp
@@ -0,0 +1,20 @@
+;;;;
+;;;; update-ed - edit a file then load it. Remember the last file worked on.
+;;;;
+
+;; set a default value for *update-ed-filename* without overwriting it if
+;; already defined.
+(defparameter *update-ed* "untitled.lisp")
+
+(defun update-ed (&optional (filename nil))
+  "update-ed - edit then load a filename.
+  Args: filename (optional), defaults to nil (i.e. use the last value of *update-ed*)
+  Side effects: filename if not nil will update update the value of *update-ed*.
+  Returns: the results of load."
+  (progn
+      (if filename
+	(setq *update-ed* filename))
+      (format t "editting ~S" *update-ed*)
+      (ed *update-ed*)
+      (load *update-ed*)))
+
