23a35943c97cdc65b45760e8ed354842c870362b
R. S. Doiel <rsdoiel@gmail.com>
2014-01-01 23:07:19 -0800

    re-organizing some of the files and functions.

--- cl-immersive/immersive.lisp
+++ cl-immersive/immersive.lisp
@@ -1,6 +1,6 @@
 ;;;;
-;;;; immersive.lisp - a light wrapper around the Unix environment to help
-;;;; create an immersive Lisp experience.
+;;;; immersive.lisp - bootstrap our Immersive Lisp environment for
+;;;; the Raspberry Pi.
 ;;;;
 ;;;; Author: R. Doiel, <rsdoiel@gmail.com>
 ;;;; copyright (c) 2013
@@ -8,33 +8,6 @@
 ;;;; See http://opensource.org/licenses/BSD-2-Clause for details.
 ;;;;
 
-;;;
-;;; immersive the command that loads the immersive lisp environment.
-;;;
-(defun immersive (&optional (intention "load"))
-  "immersive loads the immersive Lisp environment from $HOME directory
-  of the logged in individual.
-  Args: intention - you can default it loads either a .fas or .lisp files found
-  in $HOME/cl-immersive
-  
-  Side effects: Adds a the global space various symbols and functions.
-  Returns the results of executing $HOME/cl-immersive/setup.lisp."
-
-  (let ((target (concatenate 'string (ext:getenv "HOME") "/cl-immersive/setup"))
-	(target-lisp (concatenate 'string (ext:getenv "HOME") "/cl-immersive/setup.lisp")))
-    (if (string= intention "load")
-      (load target))
-    (if (string= intention "lisp")
-      (load target-lisp))
-    (if (string= intention "compile")
-      (progn
-	(compile-file target-lisp)
-	(load target)))
-    (cl-immersive-setup intention)))
-
-;;;
-;;; Common functions used in immersive Lisp
-;;;
 
 ;;
 ;; symbol-name-to-lowercase-string - converts a symbol name into a lowercase
@@ -52,3 +25,88 @@
 	  (string-downcase (symbol-name symbol-or-string))))
   symbol-or-string)
 
+;; Convert a pathname type to a string type (optionally minus the filename extention),
+;; #P and the quotes.
+(defun pathname-to-string (filename-as-pathname &optional (extname nil))
+  "Converts a pathname to a string with or without the filename's file extension.
+  Args: filename-as-pathname, extname (defaults to nil)
+     
+    + filename-as-pathname would be the type returned for (directory ...)
+    + extname would be a string such as _.lisp_ including the leading period or nil
+    if the extention needs to remain untouched.
+
+   Returns a string version of the pathname."
+  (let ((fname-string nil))
+    (progn
+      (setq fname-string (format nil "~S" 
+				 (symbol-name-to-lowercase-string filename-as-pathname)))
+      (if extname
+	(subseq fname-string 3 (- (length fname-string) (1+ (length extname))))
+	(subseq fname-string 3 (1- (length fname-string)))))))
+
+;;
+;; Load or Compile then load a file based on an intention.
+;; The intentions understood are to load what's available
+;; preferring the .fas file, explicity load the .lisp version
+;; or compile the .lisp version and load the resulting .fas file.
+;;
+(defun smart-load (fname-string intention)
+  "Args: fname-string, intention
+
+    + fname-string would be a string version of a pathname *without a filename extention*.
+    + intention is a string of either *compile*, *lisp* or *load*.
+
+  Returns the results of (load ...)"
+    (progn
+      (if (string= intention "compile")
+	(compile-file (concatenate 'string 
+				   (symbol-name-to-lowercase-string fname-string) ".lisp")))
+      (if (string= intention "lisp")
+	  (load (concatenate 'string 
+			     (symbol-name-to-lowercase-string fname-string) ".lisp"))
+	  (load (symbol-name-to-lowercase-string fname-string)))))
+
+;;
+;; Scan this immersive folder and load the lisp or fas files we find.
+;; If requested compile them first.
+;;
+(defun immersive-init (&optional (intention "load"))
+  "Args: intention is a string that is either load, lisp, compile. The default is load.
+  	load - will load either .fas or if not found the .lisp file
+  	lisp - will explicitly load the .lisp files only
+  	compile - will compile the lisp files and then load the .fas rendered.
+  Returns results of loadnig the list of files."
+  (let ((fname-as-string nil))
+    ; Now process all the parts, make a list of files and compile or load them
+    (dolist (filename-as-pathname (directory (concatenate 'string (ext:getenv "HOME") "/cl-immersive/*.lisp")))
+      (setq fname-as-string (pathname-to-string filename-as-pathname ".lisp"))
+      (if (equal (search "/setup" fname-as-string) ()) 
+	(smart-load fname-as-string intention)))))
+
+;;;
+;;; immersive the command that loads the immersive lisp environment.
+;;;
+(defun immersive (&optional (intention "load"))
+  "immersive loads the immersive Lisp environment from $HOME directory
+  of the logged in individual.
+  Args: intention - you can default it loads either a .fas or .lisp files found
+  in $HOME/cl-immersive
+  
+  Side effects: Adds a the global space various symbols and functions.
+  Returns the results of executing $HOME/cl-immersive/init.lisp."
+
+  (let ((target (concatenate 'string (ext:getenv "HOME") "/cl-immersive/init"))
+	(target-lisp (concatenate 'string (ext:getenv "HOME") "/cl-immersive/init.lisp")))
+    (if (string= intention "load")
+      (load target))
+    (if (string= intention "lisp")
+      (load target-lisp))
+    (if (string= intention "compile")
+      (progn
+	(compile-file target-lisp)
+	(load target)))
+    (immersive-init intention)))
+
+;;;
+;;; Common functions used in immersive Lisp
+;;;
---
+++ cl-immersive/init.lisp
@@ -0,0 +1,15 @@
+;;;
+;;; cl-immserive/init.lisp - bootstrap my immersive lisp environment for 
+;;; _ecl_ and _Raspbery Pi_
+;;;
+;;; Setup an immersive Lisp environment for Raspberry Pi
+;;; Hosted by the [ecl]() Lisp implementation, cl-immersive will be able to leverage
+;;; Most prior work done in Unix and still provide a rich Lisp environment to
+;;; build application and explore your Raspberry Pi.
+;;;
+
+;; Load immersive.lisp
+(load "immersive.lisp")
+;; Now initialize the system
+(immersive)
+
