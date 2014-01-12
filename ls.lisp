632920a153fc053b1d7183d3205c76fd30918da8
R. S. Doiel <rsdoiel@gmail.com>
2014-01-11 22:47:19 -0800

    snapshot

--- ls.lisp
+++ ls.lisp
@@ -1,9 +1,13 @@
 ;;
 ;; ls - wrap the Unix ls command
 ;;
-(defun ls (&optional (path-filter "*.*"))
-  "Args: path-filter provides for the path regex you'd use with ls.
-  Other options to be added later"
-  (princ (shell (concatenate 'string "ls --color -la " 
-			     (convert-symbol-or-pathname path-filter)))))
+(defun ls (&optional (folder ".") (name "*") (type "*"))
+  "list the contents of a directory.
+  Args: folder [absolute or relative path], name [base name of file], type [filename extension, e.g. lisp]
+  Returns: A list of files and directories"
+  (let ((path-type (if (string= (subseq folder 0 1) "/")
+		     ':absolute
+		     ':relative)))
+    (concatenate 'list (directory (make-pathname :directory `(,path-type ,folder :wild)))
+		 (directory (make-pathname :directory `(,path-type ,folder) :name name :type type)))))
 
