6cc68b7c184affb29840516fe27cc9af4bd2b98c
R. S. Doiel <rsdoiel@gmail.com>
2014-01-15 21:20:15 -0800

    ideas

--- pipes.lisp
+++ pipes.lisp
@@ -2,4 +2,18 @@
 ;;;; pipes.lisp two functions to.emmulate the pipe symbol and tee program.
 ;;;;
 
+;;;
+;;; pipe-line
+;;;
+(defun pipe-line (input-stream &rest output-streams)
+  "sequencial have each out process the input of the precious function"
+  nil)
+
+;;;
+;;; pipe-tee
+;;;
+(defun pipe-tee (input-stream &rest output-streams)
+  " accepts an input stream and sends a copy of the result to each 
+  item in the output list."))
+  nil)
 
