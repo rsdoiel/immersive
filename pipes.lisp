43c4e1087b98dfadb82e9a1f9e076937d63f6fa4
R. S. Doiel <rsdoiel@gmail.com>
2014-01-20 00:33:09 -0800

    snapshot

--- pipes.lisp
+++ pipes.lisp
@@ -6,7 +6,7 @@
 ;;;
 ;;; pipe-line
 ;;;
-(defun pipe-line (input &rest operations) 
+(defun pipe-line (input &rest operations)
   "Pipe line processes a text through sequencial list of functions
   taking the output text of the previous as the input of the next.
   
@@ -14,7 +14,9 @@
      input - the text stream to process
      operations - a list of functions to call and apply to input text
   Returns: a value list with first element a string, a list or nil"
-  nil)
+  (progn
+    (print "pipe-line not implemented.")
+    nil))
 
 ;;;
 ;;; pipe-clone
@@ -26,16 +28,20 @@
     input - the text stream to split
     operations - a list of function names to sent the text through
   Returns: a value list with first element a string, a list or nil"
-  nil)
+  (progn
+    (print "pipe-fork not implmented.")
+    nil))
 
 ;;;
 ;;; pipe-merge
 ;;;
-(defun pipe-merge (inputs, operation)
+(defun pipe-merge (inputs operation)
   "Merge a list of inputs and apply an operation
   Args: 
     inputs - a list of texts
     operation - a function to process the concatentated inputs
   Returns: a value list with first element a string, a list or nil"
-  nil)
+  (progn
+    (print "pipe-merge not implemented.")
+    nil))
 
