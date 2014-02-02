cf57ac420620c1d5d3a060a2013532361ac99a27
R. S. Doiel <rsdoiel@yahoo.com>
2014-02-01 21:46:24 -0800

    snapshot

--- package.lisp
+++ package.lisp
@@ -2,5 +2,11 @@
 
 (defpackage #:immersive
   (:use #:cl)
-  (:export :cd :pwd :ls :update-edit :edit :mkdir :rmdir))
+  (:export :cd :pwd :ls :update-edit 
+	   :edit :mkdir :rmdir
+	   :git-status :git-add
+	   :git-commit :git-push
+	   :git-pull :git-push-origin-master
+	   :git-scm
+	   :cat))
 
